719c15b609293befc69bced17081b01b08df7c4c
fangaxeeeel <160501610+fangaxeeeel@users.noreply.github.com>
2025-02-24 22:38:47 +0800

    Updated urls.py and settings.py in recipebook, as well as urls.py and views.py in ledger app. Added also recipe_detail.html and recipe_list.html in templates.

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def recipe_list(request):
+    template = loader.get_template('recipe_list.html')
+    context = {
+        "recipes": [
+            {"name": "Recipe 1", "link": "/recipe/1/"},
+            {"name": "Recipe 2", "link": "/recipe/2/"}
+        ]
+    }
+    return HttpResponse(template.render(context, request))
+
+def recipe_detail(request, num):
+    recipes = {
+        1: {
+            "name": "Recipe 1",
+            "ingredients": [
+                {"name": "tomato", "quantity": "3pcs"},
+                {"name": "onion", "quantity": "1pc"},
+                {"name": "pork", "quantity": "1kg"},
+                {"name": "water", "quantity": "1L"},
+                {"name": "sinigang mix", "quantity": "1 packet"}
+            ],
+            "link": "/recipe/1/"
+        },
+        2: {
+            "name": "Recipe 2",
+            "ingredients": [
+                {"name": "garlic", "quantity": "1 head"},
+                {"name": "onion", "quantity": "1pc"},
+                {"name": "vinegar", "quantity": "1/2 cup"},
+                {"name": "water", "quantity": "1 cup"},
+                {"name": "salt", "quantity": "1 tablespoon"},
+                {"name": "whole black peppers", "quantity": "1 tablespoon"},
+                {"name": "pork", "quantity": "1 kilo"}
+            ],
+            "link": "/recipe/2/"
+        }
+    }
+
+    recipe = recipes.get(num, {"name": "No Recipe Found", "ingredients": [], "link": "#"})
+
+    template = loader.get_template('recipe_detail.html')
+    return HttpResponse(template.render({"recipe": recipe}, request))
