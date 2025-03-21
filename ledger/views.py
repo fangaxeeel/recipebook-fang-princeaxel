b0e91606100ee833eceba693ebbc1ade9454f449
fangaxeeeel <160501610+fangaxeeeel@users.noreply.github.com>
2025-03-21 19:20:57 +0800

    Added the accounts/ path in urls.py in the project-level urls.py to account for the login and logout paths of the project. Added also the default login and logout pages, and made the views for recipe_detail and recipe_list login required using the @login_required decorator.

--- ledger/views.py
+++ ledger/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    return render(request, 'recipe_list.html', {'recipes': recipes})
+    return render(request, "recipe_list.html", {"recipes": recipes})
 
+@login_required
 def recipe_detail(request, num):
     recipe = Recipe.objects.get(id=num)
-    return render(request, 'recipe_detail.html', {'recipe': recipe})
+    return render(request, "recipe_detail.html", {"recipe": recipe})
